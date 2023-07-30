f3dce5cce49c8ddec1edbf2e370f9b4b4f5e89bf
Janisroz <janisrozkalejs@gmail.com>
2023-07-30 11:05:07 +0000

    Create intro section and name function validating the input

--- run.py
+++ run.py
@@ -1,3 +1,38 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def name():
+    """
+    Gets user name and validates input
+    """
+    while True:
+        name = input("Please Enter your first name:\n")
+        name = name.lower()
+
+        if validate_name(name):
+            print("name is valid")
+            break
+
+    return name
+
+
+def validate_name(name):
+    try:
+        if str.isalpha(name) and len(name)< 20:
+            print("String is good")
+        else:
+            raise ValueError(
+                f"Name must only be letters and less than 20 characters"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again\n")
+        return False
+
+    return True
+
+def main():
+    name()
+
+
+print("Welcome to Fit Check!")
+print("We will ask you some questions, do some calculations and then give some recommendations")
+print("But first...\n")
+main()
